05f629977ae9dbbd7d42a4df0f0d13d75c6bd832
Kenneth <taleswithdata@gmail.com>
2025-10-22 21:48:23 +0100

    added dockerignore

--- app/core/config.py
+++ app/core/config.py
@@ -3,10 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Settings:
     PROJECT_NAME: str = "String Analyzer Service"
-    DATA_FILE: str = os.getenv("DATA_FILE", "data.json")
-
+    DATA_FILE: str = os.getenv("DATA_FILE", "/tmp/data.json")
+    PORT: int = int(os.getenv("PORT", 8000))
 
 settings = Settings()
